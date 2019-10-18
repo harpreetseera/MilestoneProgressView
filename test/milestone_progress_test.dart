5e7c9f475eade4ac333108550094a3c30d770f64
harpreet8525 <harpreet@flick2know.com>
2019-10-18 13:37:21 +0530

    added test cases and github workflow

--- test/milestone_progress_test.dart
+++ test/milestone_progress_test.dart
@@ -1,13 +1,49 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:milestone_progress/milestone_progress.dart';
+import '../example/lib/main.dart';
 
 void main() {
-//  test('adds one to input values', () {
-//    final calculator = Calculator();
-//    expect(calculator.addOne(2), 3);
-//    expect(calculator.addOne(-7), -6);
-//    expect(calculator.addOne(0), 1);
-//    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
-//  });
+  group('Widget Smoke test', () {
+    testWidgets('Increment Button Test', (WidgetTester tester) async {
+      await tester.pumpWidget(MyApp());
+
+      expect(find.byTooltip('Increment'), findsOneWidget);
+      expect(find.byTooltip('decrement'), findsOneWidget);
+
+      // Ensuring counter starts at 0
+      expect(find.text('0 milestone completed'), findsOneWidget);
+
+      // Incrementing counter by +1
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pumpAndSettle();
+
+      // To ensure counter incremented by +1
+      expect(find.text('1 milestone completed'), findsOneWidget);
+    });
+
+    testWidgets('Decrement Button Test', (WidgetTester tester) async {
+      await tester.pumpWidget(MyApp());
+
+      expect(find.byTooltip('Increment'), findsOneWidget);
+      expect(find.byTooltip('decrement'), findsOneWidget);
+
+      // Ensuring counter starts at 0
+      expect(find.text('0 milestone completed'), findsOneWidget);
+
+      // Incrementing counter by +1 to test decrement
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pumpAndSettle();
+
+      // To ensure counter incremented by +1
+      expect(find.text('1 milestone completed'), findsOneWidget);
+
+      // Decrementing counter by -1
+      await tester.tap(find.byIcon(Icons.remove));
+      await tester.pumpAndSettle();
+
+      // To ensure counter decremented by -1
+      expect(find.text('0 milestone completed'), findsOneWidget);
+    });
+  });
 }
